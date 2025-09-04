8777b075e0e70a83b357cd3032659ae10e61ee30
nilssonfilip1996 <nilssonfilip1996@hotmail.com>
2025-09-04 12:12:26 +0200

    Added a header component and the Quiz component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
-function App() {}
+import Header from "./components/Header";
+import Quiz from "./components/Quiz";
+
+function App() {
+  return (
+    <>
+      <Header title={"REACTQUIZ"} />
+      <Quiz />
+    </>
+  );
+}
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header({title}) {
+    return (
+        <>
+            <header>
+                <img src="/quiz-logo.png" alt="" />
+                <h1>{title}</h1>
+            </header>
+        </>
+    )
+}
